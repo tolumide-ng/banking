4d9f6416aab946c983b4c953a1e1275ef16f65dd
tolumide-ng <tolumideshopein@gmail.com>
2023-03-02 23:29:54 +0100

    Creates bankAccount command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub enum BankAccountCommand {
+    OpenAccount { account_id: String },
+    DepositMoney { amount: f64 },
+    WithdrawMoney { amount: f64 },
+    WriteCheck { check_number: String, amount: f64 },
+}
+
+
+
+
 fn main() {
     println!("Hello, world!");
 }
+
